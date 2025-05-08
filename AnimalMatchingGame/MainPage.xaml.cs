991df24576e6027c19f8eed3e06cab92e40232bc
Anastasija <anastasija.stojkovska@quality-line.com>
2025-05-08 11:25:20 +0200

    Finished game logic and timer

--- AnimalMatchingGame/MainPage.xaml.cs
+++ AnimalMatchingGame/MainPage.xaml.cs
@@ -2,6 +2,11 @@
 {
     public partial class MainPage : ContentPage
     {
+        Button lastClicked;
+        bool findingMatch = false;
+        int matchesFound;
+        int tenthsOfSecondsElapsed = 0;
+
         public MainPage()
         {
             InitializeComponent();
@@ -39,8 +44,61 @@
                 button.Text = nextEmoji;
                 animalEmoji.RemoveAt(index);
             }
+
+            Dispatcher.StartTimer(TimeSpan.FromSeconds(.1), TimerTick);
         }
 
-        private void Button_Clicked(object sender, EventArgs e) { }
+        private bool TimerTick()
+        {
+            if (!this.IsLoaded)
+                return false;
+
+            tenthsOfSecondsElapsed++;
+
+            TimeElapsed.Text = "Time elapsed: " + (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+
+            if (PlayAgainButton.IsVisible)
+            {
+                tenthsOfSecondsElapsed = 0;
+                return false;
+            }
+            return true;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (sender is Button buttonClicked)
+            {
+                if (!string.IsNullOrWhiteSpace(buttonClicked.Text) && findingMatch == false)
+                {
+                    buttonClicked.BackgroundColor = Colors.Red;
+                    lastClicked = buttonClicked;
+                    findingMatch = true;
+                }
+                else
+                {
+                    if (
+                        (buttonClicked != lastClicked)
+                        && (buttonClicked.Text == lastClicked.Text)
+                        && (!String.IsNullOrWhiteSpace(buttonClicked.Text))
+                    )
+                    {
+                        matchesFound++;
+                        lastClicked.Text = " ";
+                        buttonClicked.Text = " ";
+                    }
+                    lastClicked.BackgroundColor = Colors.LightBlue;
+                    buttonClicked.BackgroundColor = Colors.LightBlue;
+                    findingMatch = false;
+                }
+            }
+
+            if (matchesFound == 8)
+            {
+                matchesFound = 0;
+                AnimalButtons.IsVisible = false;
+                PlayAgainButton.IsVisible = true;
+            }
+        }
     }
 }
